648a64ab308ee8497b39e16e1a203a235bf99ac0
Zikri <zikrimansyursyah@gmail.com>
2021-12-07 01:47:41 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-
 function App() {
   return (
     <div className="App">
-      <div className="home h-screen relative">
-        <nav className="bg-gray-800 h-20 lg:w-10/12 2xl:w-3/4 m-auto absolute">
+      <div className="app-home h-screen relative">
+        <nav className="bg-gray-50 bg-opacity-20 h-20 w-screen fixed">
+          <div className="m-auto text-black opacity-100 font-medium font-primary text-lg w-screen h-full lg:w-10/12 2xl:w-3/4 flex items-center justify-between">
+            <div className="w-1/6 text-3xl font-bold">
+              <a href="!#">Royal Enfield</a>
+            </div>
+            <div className="">
+              <ul className="flex gap-20">
+                <li>Home</li>
+                <li>About</li>
+                <li>Our Work</li>
+                <li>Pricing</li>
+              </ul>
+            </div>
+            <div className="w-1/6 text-right">Contact Us</div>
+          </div>
         </nav>
-        <div className="bg-hero-img h-full bg-cover">
-        </div>
+        <div className="bg-hero-img h-full bg-cover"></div>
+      </div>
+      <div className="h-60">
+        !Secara default, Tailwind menyediakan tiga utilitas keluarga font: tumpukan sans-serif lintas-browser, tumpukan
+        serif lintas-browser, dan tumpukan monospace lintas-browser. Anda dapat mengubah, menambah, atau menghapusnya
+        dengan mengedit theme.fontFamilybagian konfigurasi Tailwind Anda.
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        primary: ['"Source Sans Pro"', "sans-serif"],
+      },
       backgroundImage: {
-        'hero-img': "url('/src/img/hero.jpg')",
-      }
+        "hero-img": "url('/src/img/hero.jpg')",
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
