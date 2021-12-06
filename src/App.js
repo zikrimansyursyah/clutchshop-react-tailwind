d2f9b23c26842add46e68fa98fd3410e2dc58bf5
Zikri <zikrimansyursyah@gmail.com>
2021-12-06 15:31:39 +0700

    new

--- src/App.js
+++ src/App.js
@@ -2,18 +2,12 @@
 function App() {
   return (
     <div className="App">
-      <nav class="bg-gray-800 h-20 w-3/4 m-auto rounded-b-2xl flex justify-between items-center px-10 shadow-xl relative">
-        <div class="flex w-1/5">
-          <h2 class="text-gray-50 text-md mr-6"><a href="!#">Store</a></h2>
-          <h2 class="text-gray-50 text-md mr-6"><a href="!#">Promo</a></h2>
-          <h2 class="text-gray-50 text-md"><a href="!#">About</a></h2>
+      <div className="home h-screen relative">
+        <nav className="bg-gray-800 h-20 lg:w-10/12 2xl:w-3/4 m-auto absolute">
+        </nav>
+        <div className="bg-hero-img h-full bg-cover">
         </div>
-        <h1 class="text-3xl text-gray-50 font-sans font-semibold">Flexshop</h1>
-        <div class="flex w-1/5 justify-end items-center">
-          <a class="text-gray-50 text-lg mr-6" href="!#">Login</a>
-          <a class="text-gray-900 text-md font-medium bg-yellow-400 p-2 px-5 rounded-lg hover:bg-yellow-300" href="!#">Sign Up</a>
-        </div>
-      </nav>
+      </div>
     </div>
   );
 }
